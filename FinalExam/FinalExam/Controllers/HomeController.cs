86c0095deac85fa7bf434b47079a95a7d8eda74b
Nihad Balakisiyev <tu72l9qlw@code.edu.az>
2023-01-28 12:31:09 +0400

    update

--- FinalExam/FinalExam/Controllers/HomeController.cs
+++ FinalExam/FinalExam/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
 ﻿using FinalExam.Models;
+using FinalExam.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace FinalExam.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(DatabaseContext c) { }
+        private readonly DatabaseContext _context;
+        public HomeController(DatabaseContext context) 
+        {
+            _context=context;
+        }
         public IActionResult Index()
         {
-            
-            return View();
+            HomeViewModel homeVM = new HomeViewModel
+            {
+                TeamMembers=_context.TeamMembers.Include(x=>x.SocialMediaAccounts).OrderBy(x=>x.Order).Take(3).ToList(),
+                SocialMediaAccounts=_context.SocialMediaAccounts.OrderByDescending(x=>x.Id).Take(4).ToList(),
+            };
+            return View(homeVM);
         }
 
     }
---
+++ FinalExam/FinalExam/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FinalExam.Models;
+
+namespace FinalExam.ViewModels
+{
+    public class HomeViewModel
+    {
+       public List<TeamMember> TeamMembers { get; set; }
+       public List<SocialMediaAccount> SocialMediaAccounts { get; set; }
+    }
+}
